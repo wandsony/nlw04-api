089a4dd12ce6daa21eda4dc89b676e430810a9b2
Wandson Gomes <wandsony@gmail.com>
2021-02-23 14:50:18 -0300

    incluindo banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,11 @@
+import 'reflect-metadata'
 import express from "express";
+import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-/**
- * GET => Busca
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Aleração especifica
- */
+app.use(express.json());
+app.use(router);
 
-    //http:localhost:3333/users
- app.get("/",(request, response) =>{
-     return response.json({ message: "Hello World - NLW04" })
- });
-
- // 1 param => Rota(Recurso API)
- // 2 param => request, response
- 
-app.post("/",(request, response) =>{
-    // Recebeu os dados para salvar 
-    return response.json({ message: "Os dados foram salvos com sucesso!" })
- });
- 
 app.listen(3333, () => console.log("Server is running!"));
